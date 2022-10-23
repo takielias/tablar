345a1badcb82a7baa4a71189af380a42b834740c
takielias <taki.elias@gmail.com>
2022-10-23 12:50:29 +0600

    Added display alert config.

--- resources/views/partials/common/alert.blade.php
+++ resources/views/partials/common/alert.blade.php
@@ -1,25 +1,16 @@
 @if(Session::has('message'))
     <div class="alert alert-info" role="alert">
-        <h4 class="alert-title">Did you know?</h4>
+        <h4 class="alert-title">Info !</h4>
         <div class="text-muted">Here is something that you might like to know.</div>
     </div>
 @elseif(Session::has('success'))
     <div class="alert alert-success" role="alert">
-        <h4 class="alert-title">Wow! Everything worked!</h4>
+        <h4 class="alert-title">Success !</h4>
         <div class="text-muted">Your account has been saved!</div>
     </div>
 @elseif(Session::has('error'))
     <div class="alert alert-danger" role="alert">
-        <h4 class="alert-title">I'm so sorry&hellip;</h4>
+        <h4 class="alert-title">Error !</h4>
         <div class="text-muted">Your account has been deleted and can't be restored.</div>
     </div>
-@endif
-
-@if ($errors->any())
-    @foreach ($errors->all() as $error)
-        <div class="alert alert-danger" role="alert">
-            <h4 class="alert-title">Error !</h4>
-            <div class="text-muted">{{$error}}</div>
-        </div>
-    @endforeach
 @endif
