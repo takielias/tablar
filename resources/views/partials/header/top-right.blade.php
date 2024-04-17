dc18fdc9eeb078351dfbc145b384ba48467cdaab
takielias <taki.elias@gmail.com>
2024-04-17 08:45:27 +0600

    Added Custom Error Page Layout.

--- resources/views/partials/header/top-right.blade.php
+++ resources/views/partials/header/top-right.blade.php
@@ -2,10 +2,9 @@
     <div class="nav-item dropdown">
         <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown"
            aria-label="Open user menu">
-                        <span class="avatar avatar-sm"
-                              style="background-image: url({{asset('assets/avatars/000m.jpg')}})"></span>
+            <span class="avatar">SE</span>
             <div class="d-none d-xl-block ps-2">
-                <div>{{Auth()->user()->name}}</div>
+                <div>{{Str::firstAndLast(Auth()->user()->name)}}</div>
                 <div class="mt-1 small text-muted">Software Engineer</div>
             </div>
         </a>
